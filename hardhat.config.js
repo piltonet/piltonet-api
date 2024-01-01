e99b61e5f314958ec66c6535c6c4e1bc26371ba0
Farokh Madah <fa.madah@gmail.com>
2024-01-01 12:52:38 +0330

    back to tomotestnet /* JSON-RPC err */ + new deployments

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,15 +3,19 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
-  defaultNetwork: "victestnet",
+  defaultNetwork: process.env.DEFAULT_NETWORK,
   networks: {
+    viction: {
+      url: "https://rpc.viction.xyz",
+      accounts: [process.env.SERVICE_ADMIN_PRIVATE_KEY || ""]
+    },
     victestnet: {
       url: "https://rpc-testnet.viction.xyz",
       accounts: [process.env.SERVICE_ADMIN_PRIVATE_KEY || ""]
     },
-    vicmainnet: {
-      url: "https://rpc.viction.xyz",
-      accounts: [process.env.SERVICE_ADMIN_PRIVATE_KEY || ""]
+    tomotestnet: {
+      url: "https://rpc.testnet.tomochain.com",
+      accounts: [process.env.PRIVATE_KEY || ""]
     }
   }
 };
