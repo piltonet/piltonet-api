04bea02a295765f799c73a79e1c8c4a3b3cddae2
Farokh Madah <fa.madah@gmail.com>
2024-02-06 01:53:04 +0330

    clean-up

--- api.js
+++ api.js
@@ -9,7 +9,6 @@ var app = api.config();
 global._LANG = app.language;
 
 //Running app is server instance to serve clients
-//app.use('/documentation',docs.get_doc());
 const SERVER = app.express.listen(process.env.NODE_PORT, () => {
  console.log('Server running on port: ', process.env.NODE_PORT);
 });
